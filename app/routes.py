09613787479796b8d138aa91ac61a5a7ce4a0037
Nikita Arapov <nicarpov@gmail.com>
2025-05-27 16:20:45 +0300

    login flashed messages and login form error messages added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, flash
 from app import data
 from app import app
 from app.forms import LoginForm
@@ -14,7 +14,12 @@ def exercises():
     
     return render_template('exercises.html', title="Exercises", exercises=data.exercises)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        flash("{}".format(form.username.data))
+        flash("{}".format(form.username.data))
+        return redirect('/index')
     return render_template('login.html', title='Вход', form=form)
