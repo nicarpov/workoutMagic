51952cc85a9dd94b03342289b40606515048900c
Nikita Arapov <nicarpov@gmail.com>
2025-05-28 14:27:48 +0300

    flask_sqlalchemy and flask_migrate connected to the app

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 from dotenv import load_dotenv
 import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+     
