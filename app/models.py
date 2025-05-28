ce60b22768d219ff252db299e3d889523972b9f9
Nikita Arapov <nicarpov@gmail.com>
2025-05-28 14:47:15 +0300

    models.py User class error fixed

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(32), unique=True, index=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True, index=True)
-    password_hash: so.Mapped[str] = so.mapped_column(Optional[sa.String(256)])
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     def __repr__(self):
         return "<User {}".format(self.username) 
