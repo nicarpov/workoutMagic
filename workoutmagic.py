3a05e8c5d6b1d4696b2e1eed64a5d3d7da2b02d1
Nikita Arapov <nicarpov@gmail.com>
2025-05-29 15:18:26 +0300

    flask shell context function added to workoutmagic.py

--- workoutmagic.py
+++ workoutmagic.py
@@ -1,1 +1,11 @@
-from app import app
+from app import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app.models import User, Workout, Exercise
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"app": app, "db": db, "sa": sa, "so": so, \
+        "User": User, "Workout": Workout, "Exercise": Exercise}
+    
